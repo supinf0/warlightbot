2f3fbbe99fbcd80898df0a884e01cce41dc234fa
supinf0 <f0x01@web.de>
2016-11-05 10:19:31 +0100

    cargo test, new project

---
+++ rust_tests/experimental/src/main.rs
@@ -0,0 +1,26 @@
+
+
+
+/// List of things you need to learn:
+///  higher order functions (?)
+///  print to stderr
+///  random numbers
+///  iterators/for loop
+///  traits
+///  use multiple .rs files
+///  ...
+/// 
+/// 
+/// 
+/// 
+/// 
+
+fn main() {
+    println!("Hello, world!");
+    println!(" fun1() returns {} as a value",fun1());
+}
+
+
+fn fun1() -> i32 {
+ 2
+}
