a3f9f0c3eb9db44f2111a96308908e2811da2116
supinf0 <f0x01@web.de>
2016-11-14 08:30:55 +0100

    add overflow_test()

--- rust_tests/experimental/src/main.rs
+++ rust_tests/experimental/src/main.rs
@@ -8,19 +8,28 @@
 ///  iterators/for loop
 ///  traits
 ///  use multiple .rs files
-///  ...
-/// 
-/// 
-/// 
-/// 
-/// 
+///  tuples
+///  function pointers
+///  loop lables
+///
+///
+///
 
 fn main() {
     println!("Hello, world!");
     println!(" fun1() returns {} as a value",fun1());
+    let a:i8 = overflow_test();
+    println!(" overflow_test() returns {} as a value",a);
 }
 
 
 fn fun1() -> i32 {
  2
 }
+
+fn overflow_test() -> i8 {
+  let a:i8 = 125;
+  a+12  // note: this behaves different if compiled with --release or not
+}
+
+
