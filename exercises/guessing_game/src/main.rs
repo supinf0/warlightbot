33b277fda05823766f07ee1ed7043d6aba444fcc
ShinWhite <erichuster@web.de>
2016-11-20 15:31:06 +0100

    exercises in formatting

--- exercises/guessing_game/src/main.rs
+++ exercises/guessing_game/src/main.rs
@@ -7,30 +7,30 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-  println!("Guess the number!");
-  let secret_number = rand::thread_rng().gen_range(1,101);
+        println!("Guess the number!");
+        let secret_number = rand::thread_rng().gen_range(1,101);
 
-  loop {
-    println!("Input: ");
+        loop {
+                println!("Input: ");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-      .expect("Failed to read line");
+                let mut guess = String::new();
+                io::stdin().read_line(&mut guess)
+                        .expect("Failed to read line");
 
-    let guess: u32 = match guess.trim().parse() {
-      Ok(num) => num,
-      Err(_) => continue
-    };
+                let guess: u32 = match guess.trim().parse() {
+                        Ok(num) => num,
+                                Err(_) => continue
+                };
 
-    println!("You guessed: {}", guess);
+                println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-      Ordering::Less => println!("Too small!"),
-      Ordering::Greater => println!("Too big!"),
-      Ordering::Equal => { 
-        println!("You win!");
-        break;
-      }
-    }
-  }
+                match guess.cmp(&secret_number) {
+                        Ordering::Less => println!("Too small!"),
+                                Ordering::Greater => println!("Too big!"),
+                                Ordering::Equal => { 
+                                        println!("You win!");
+                                        break;
+                                }
+                }
+        }
 }
